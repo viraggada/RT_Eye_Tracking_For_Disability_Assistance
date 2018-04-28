023879f55778be8f88904e3662b56eadd8204dec
Virag Gada <virag.gada@colorado.edu>
2018-04-27 19:06:35 -0600

    Adding initial files

---
+++ main.cpp
@@ -0,0 +1,88 @@
+/**********************************************************
+* Authors: Virag Gada and Shreyas Vasanhkumar
+* Title: Real-time eye tracking for disability assisatance
+* File Name: main.c
+**********************************************************/
+
+#include <stdlib.h>
+#include <pthread.h>
+#include <sched.h>
+#include <semaphore.h>
+#include "main.h"
+
+int main(int argc, const char** argv)
+{
+  struct timeval current_time_val;
+  int i, rc, scope;
+  cpu_set_t threadcpu;
+  pthread_t threads[NUM_THREADS];
+  threadParams_t threadParams[NUM_THREADS];
+  pthread_attr_t rt_sched_attr[NUM_THREADS];
+  int rt_max_prio, rt_min_prio;
+  struct sched_param rt_param[NUM_THREADS];
+  struct sched_param main_param;
+  pthread_attr_t main_attr;
+  pid_t mainpid;
+  cpu_set_t allcpuset;
+
+  printf("System has %d processors configured and %d available.\n", get_nprocs_conf(), get_nprocs());
+
+  CPU_ZERO(&allcpuset);
+
+  for(i=0; i < NUM_CPU_CORES; i++)
+     CPU_SET(i, &allcpuset);
+
+  printf("Using CPUS=%d from total available.\n", CPU_COUNT(&allcpuset));
+
+
+  // initialize the sequencer semaphores
+  //
+  if (sem_init (&semS1, 0, 0)) { printf ("Failed to initialize S1 semaphore\n"); exit (-1); }
+  if (sem_init (&semS2, 0, 0)) { printf ("Failed to initialize S2 semaphore\n"); exit (-1); }
+  if (sem_init (&semS3, 0, 0)) { printf ("Failed to initialize S3 semaphore\n"); exit (-1); }
+  if (sem_init (&semS4, 0, 0)) { printf ("Failed to initialize S4 semaphore\n"); exit (-1); }
+  if (sem_init (&semS5, 0, 0)) { printf ("Failed to initialize S5 semaphore\n"); exit (-1); }
+
+  mainpid = getpid();
+
+  rt_max_prio = sched_get_priority_max(SCHED_FIFO);
+  rt_min_prio = sched_get_priority_min(SCHED_FIFO);
+
+  rc=sched_getparam(mainpid, &main_param);
+  main_param.sched_priority=rt_max_prio;
+  rc=sched_setscheduler(getpid(), SCHED_FIFO, &main_param);
+  if(rc < 0) perror("main_param");
+  print_scheduler();
+
+
+  pthread_attr_getscope(&main_attr, &scope);
+
+  if(scope == PTHREAD_SCOPE_SYSTEM)
+    printf("PTHREAD SCOPE SYSTEM\n");
+  else if (scope == PTHREAD_SCOPE_PROCESS)
+    printf("PTHREAD SCOPE PROCESS\n");
+  else
+    printf("PTHREAD SCOPE UNKNOWN\n");
+
+  printf("rt_max_prio=%d\n", rt_max_prio);
+  printf("rt_min_prio=%d\n", rt_min_prio);
+
+  for(i=0; i < NUM_THREADS; i++)
+  {
+
+    CPU_ZERO(&threadcpu);
+    CPU_SET(3, &threadcpu);
+
+    rc=pthread_attr_init(&rt_sched_attr[i]);
+    rc=pthread_attr_setinheritsched(&rt_sched_attr[i], PTHREAD_EXPLICIT_SCHED);
+    rc=pthread_attr_setschedpolicy(&rt_sched_attr[i], SCHED_FIFO);
+    //rc=pthread_attr_setaffinity_np(&rt_sched_attr[i], sizeof(cpu_set_t), &threadcpu);
+
+    rt_param[i].sched_priority=rt_max_prio-i;
+    pthread_attr_setschedparam(&rt_sched_attr[i], &rt_param[i]);
+
+    threadParams[i].threadIdx=i;
+  }
+
+
+}
---
+++ main.h
@@ -0,0 +1,23 @@
+/**********************************************************
+* Authors: Virag Gada and Shreyas Vasanhkumar
+* Title: Real-time eye tracking for disability assisatance
+* File Name: main.h
+**********************************************************/
+
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>
+#include <stdio.h>
+#include <iostream>
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/calib3d/calib3d.hpp>
+#include <opencv2/contrib/contrib.hpp>
+#include <opencv2/objdetect/objdetect.hpp>
+
+#define NUM_THREADS ()
+
+#endif
